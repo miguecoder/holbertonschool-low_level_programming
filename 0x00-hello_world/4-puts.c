8c670142e55f354f59274b83698e2ee41fecfb9c
miguecoder <mmarqzm@gmail.com>
2022-02-10 14:36:23 -0800

    first code in C

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,4 +1,9 @@
 #include<stdio.h>
+/**
+ * main - print -> Porgraming is like building a multilingual puzzle
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
