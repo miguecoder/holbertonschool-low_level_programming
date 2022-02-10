7db34d4072eaaf7dabd14bc2aab67ffb5851bcc2
miguecoder <mmarqzm@gmail.com>
2022-02-10 15:04:19 -0800

    Program that print

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+/**
+ * main - print -> with proper grammar, but the outcome is a piece of art,
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	puts("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
