588231d1b295f19ffa887b9848c687c607dff2c5
miguecoder <mmarqzm@gmail.com>
2022-02-11 20:32:17 -0800

    Alphabet reversewq

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - print -> print the last digit of the number stored in the variable n.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char letra;
+
+	for (letra = 'z'; letra >= 'a'; letra--)
+		putchar (letra);
+	putchar('\n');
+	return (0);
+}
+
