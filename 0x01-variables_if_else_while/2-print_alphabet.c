3d8937fbed6d509baf34590cfc6e196c0ec15dbd
miguecoder <mmarqzm@gmail.com>
2022-02-11 17:11:08 -0800

    prints the alphabet in lowercase

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,20 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - print -> print the last digit of the number stored in the variable n.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char letra;
+
+	for (letra = 'a'; letra <= 'z'; letra++)
+		putchar (letra); /*Almacena una letra entre a - z, para cada ciclo*/
+	putchar('\n');
+	return (0);
+}
