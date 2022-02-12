d6e277687a14903c85b9b1af7e1dbb5f27ed9ebf
miguecoder <mmarqzm@gmail.com>
2022-02-11 17:50:27 -0800

    Print alphabet except q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+
+/**
+ * main - print -> print the last digit of the number stored in the variable n.
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+
+{
+	char letra;
+
+	for (letra = 'a'; letra <= 'z'; letra++)
+		if (letra != q && letra != e)
+			putchar (letra); /*Almacena una letra entre a - z, para cada ciclo*/
+	putchar('\n');
+	return (0);
+}
