000d63dd0fc40b4ac29b5af127776638a9942721
miguecoder <mmarqzm@gmail.com>
2022-02-17 12:36:56 -0800

    Prints most numbers, from 0 to 9, not 2 and 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -4,13 +4,13 @@
  * print_numbers - function that prints the numbers, from 0 to 9.
  */
 
-void print_numbers(void)
+void print_most_numbers(void)
 {
 	int i;
 
 	for (i = '0' ; i <= '9' ; i++)
 	{
-		if (i != 2 && i != 4)
+		if (i != '2' && i != '4')
 		{
 			_putchar(i);
 		}
