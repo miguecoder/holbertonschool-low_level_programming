d5d0e942ea5de00ee329278fae241e79d58d7402
miguecoder <mmarqzm@gmail.com>
2022-02-17 11:37:19 -0800

    checks for a digit (0 through 9).

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -10,8 +10,8 @@
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
-	{
+	if (c >= '0' && c <= '9')/*las comillas sonpara que lo tome como número*/
+	{/*No como codigo ASCII, si es ASCII escribirria c>=48 && c<=57 */
 		return (1);
 	}
 	else
