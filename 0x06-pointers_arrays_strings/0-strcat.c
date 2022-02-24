11312d90d0c3922a9912c977a91c31b208a0ec31
miguecoder <mmarqzm@gmail.com>
2022-02-24 12:29:06 -0800

    Concatenate

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * _strcat - concatenar two integers.
+ *
+ * @dest: array tthe char, with 98 spaces
+ *
+ * @src: array to concatenate
+ *
+ * Return: dest
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a = 0;
+	int b;
+
+	while (*dest++ != '\0')
+	{
+		a++;
+	}
+	b = 0;
+	while (*src++ != '\0')
+	{
+		dest[a] = src[b];
+		a++;
+		b++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
