50d4856cff3ea6f853b58c8e68a1883c071c13e3
miguecoder <mmarqzm@gmail.com>
2022-02-24 20:41:34 -0800

    function that changes all lowercase letters of a string to uppercase.

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,10 +1,20 @@
 #include "main.h"
 /**
- * reverse_array - function that compares two strings.
- * @a: array of integers to reverses
- * @n: number of elements of array
- * Return:
+ * string_toupper - funtion that change all lowercase to uppercase.
+ * @n: string to change.
+ * Return: n changed
  */
-void reverse_array(int *a, int n)
+char *string_toupper(char *n)
 {
+	int i = 0;
+
+	while (n[i] != '\0')
+	{
+		if (n[i] >= 97 && n[i] <= 122)
+		{
+			n[i] = n[i] - 32;
+		}
+	i++;
+	}
+	return (n);
 }
