acb3d0451488e96ccc121e15812240ab5fa5b1b2
Miguel Marquez <98714246+miguecoder@users.noreply.github.com>
2022-03-04 18:58:23 -0500

    isupper

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,5 +1,21 @@
 #include "main.h"
+
+/**
+ * _isupper - Function that checks for uppercase character.
+ *
+ * @c: This is a letter ramdom
+ *
+ * Return: 1 if c is uppercase, 0 in otherwise
+ */
+
 int _isupper(int c)
 {
-	return (0);
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
