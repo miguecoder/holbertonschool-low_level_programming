52e1592f1bb5b0b3c30bed5514319c1bb8fb3339
Miguel Marquez <98714246+miguecoder@users.noreply.github.com>
2022-03-04 19:12:07 -0500

    islower

--- 0x09-static_libraries/3-islower.c
+++ 0x09-static_libraries/3-islower.c
@@ -1,5 +1,21 @@
 #include "main.h"
+/**
+ * _islower - (int c) checks for lowercase character.
+ *
+ * @c: Is the parameter (one letter that  can be lowercase or uppercase)
+ *
+ * Return: 1 if is lowercase, 0 if otherwise
+ *
+ */
+
 int _islower(int c)
 {
-	return (0);
+	if (c >= 'a' && c <= 'z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }
