ed579ad5b837a261fa224afb7be7adec22864228
miguecoder <mmarqzm@gmail.com>
2022-03-17 13:03:20 -0700

    Function that prints a name

---
+++ 0x0F-function_pointers/1-array_iterator.c
@@ -0,0 +1,15 @@
+#include "function_pointers.h"
+/**
+ * print_nam - Function that prints name.
+ * @name: name to print
+ * @f: pointer to function.
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name == NULL || f == NULL)
+	{
+		;
+	}
+	f(name);
+}
