97957bd3238dd26725abce6a51c595f732531927
miguecoder <mmarqzm@gmail.com>
2022-03-29 21:01:59 -0700

    add prototype of before_main

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -28,4 +28,5 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 int _strlen(const char *string);
+void before_main(void);
 #endif /*LISTS_H*/
