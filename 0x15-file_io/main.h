505cbdd00f92912c45ee932b3c12ec85e2f117ae
miguecoder <mmarqzm@gmail.com>
2022-04-05 11:58:24 -0700

    function that reads a text file and prints it to the POSIX standard output.

--- 0x15-file_io/main.h
+++ 0x15-file_io/main.h
@@ -3,22 +3,22 @@
 
 /*Libraries*/
 #include <stdio.h>
-/**
+/*
  * int dprintf(int fd, const char *format, ...);
  */
 #include <stdlib.h>
-/**
+/*
  * void *malloc(size_t size);
  * void free(void *ptr);
  * callos, realloc, reallocarray..
  */
 #include <fcntl.h>
 #include <sys/stat.h>
-/**
+/*
  * int open(const char *path, int oflag, ...);
  */
 #include <unistd.h>
-/**
+/*
  * ssize_t read(int fildes, void * buf, size_t nbytes);
  * ssize_t write(int fildes, const void *buf, size_t nbyte);
  * int close(int fildes);
