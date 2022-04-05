dc80c62054491994497363e63c597813e5c45550
miguecoder <mmarqzm@gmail.com>
2022-04-05 12:51:18 -0700

    function that creates a file.

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -16,18 +16,18 @@ int create_file(const char *filename, char *text_content)
 
 	if (text_content == NULL)
 	{
-		op = open(filename, O_CREAT, 00600);
+		op = open(filename, O_CREAT, 0600);
 		if (op == -1)
 			return (0);
 		close(op);
 		return (1);
 	}
 
-	op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 00600);
+	op = open(filename, O_CREAT | O_WRONLY | O_TRUNC, 0600);
 	if (op == -1)
 		return (-1);
 
-	while (*text_content)
+	while (text_content != '\0')
 		length++;
 
 	wr = write(op, text_content, length);
