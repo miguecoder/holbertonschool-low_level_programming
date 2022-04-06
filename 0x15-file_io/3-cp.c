5df0d71f4a298e53ac612e6b8430d4a44cf79f17
miguecoder <mmarqzm@gmail.com>
2022-04-05 20:32:07 -0700

    program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -11,15 +11,15 @@ int main(int argc, char *argv[])
 	char buffer[1024];
 
 	if (agrc != 3)
-		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n", exit(97));
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
 	op1 = open(agrv[1], O_RDONLY);
 	if (op1 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arg[1], exit(98));
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arg[1]), exit(98);
 
 	op2 = open(agrv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (op2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", arg[2], exit(99));
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", arg[2]), exit(99);
 
 	do {
 		rd1 = read(op1, buffer, 1024);
@@ -31,14 +31,14 @@ int main(int argc, char *argv[])
 
 		wr2 = write(op2, buffer, rd1);
 		if (w2 == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", arg[2]);
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", arg[2]), exit(99);
 	} while (rd1 == 1024);
 
 	cl1 = close(op1);
 	cl2 = close(op2);
 	if (cl1 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", op1, exit(100));
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", op1), exit(100);
 	if (cl2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", op2, exit(100));
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", op2), exit(100);
 	return (0);
 }
