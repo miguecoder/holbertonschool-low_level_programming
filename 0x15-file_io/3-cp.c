ba468b0faafc15a7f34fe8fa50c02f359267d10b
miguecoder <mmarqzm@gmail.com>
2022-04-05 20:40:05 -0700

    program that copies the content of a file to another file.

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -13,25 +13,28 @@ int main(int argc, char *argv[])
 	if (agrc != 3)
 		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n"), exit(97);
 
-	op1 = open(agrv[1], O_RDONLY);
+	op1 = open(argv[1], O_RDONLY);
 	if (op1 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arg[1]), exit(98);
+	{
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
+		exit(98);
+	}
 
-	op2 = open(agrv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	op2 = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 	if (op2 == -1)
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", arg[2]), exit(99);
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 
 	do {
 		rd1 = read(op1, buffer, 1024);
 		if (rd1 == -1)
 		{
-			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", arg[1]);
+			dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 			exit(98);
 		}
 
 		wr2 = write(op2, buffer, rd1);
-		if (w2 == -1)
-			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", arg[2]), exit(99);
+		if (wr2 == -1)
+			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]), exit(99);
 	} while (rd1 == 1024);
 
 	cl1 = close(op1);
