e64225e8f805ab7515e98232f8ca1585574d0db0
miguecoder <mmarqzm@gmail.com>
2022-05-13 11:55:15 -0500

    function that returns the number of elements in a linked dlistint_t list.

--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -18,5 +18,9 @@ typedef struct dlistint_s
 	struct dlistint_s *prev;
 	struct dlistint_s *next;
 } dlistint_t;
+size_t print_listint(const listint_t *h);
+listint_t *add_nodeint_end(listint_t **head, const int n);
+void free_listint(listint_t *head);
+size_t dlistint_len(const dlistint_t *h);
 size_t print_dlistint(const dlistint_t *h);
 #endif
