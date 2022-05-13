3801ff98c15372e65c408e6d8918bfe2dc6d4ef1
miguecoder <mmarqzm@gmail.com>
2022-05-13 14:17:42 -0500

    function that adds a new node at the end of a dlistint_t list.

---
+++ 0x17-doubly_linked_lists/3-add_dnodeint_end.c
@@ -0,0 +1,39 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * add_dnodeint - function that adds a new node at the beginning
+ * of a dlistint_t list.
+ * @head:pointer to a struct list
+ * @n: new integer add to a new node
+ * Return: The number of nodes
+ */
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
+{
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *new_head;
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+	new->n = n;
+	new->next = NULL;
+	new->prev = NULL;
+	if (*head == NULL)
+	{
+		*head = new;
+		return (new);
+	}
+	new_head = *head;
+	while (new_head->next != NULL)
+	{
+		new_head = new_head->next;
+	}
+	new->prev = new_head;
+	new_head->next = new;
+	return (new);
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -20,7 +20,7 @@ typedef struct dlistint_s
 } dlistint_t;
 size_t print_listint(const dlistint_t *h);
 dlistint_t *add_dnodeint(dlistint_t **head, const int n);
-dlistint_t *add_nodeint_end(dlistint_t **head, const int n);
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n);
 void free_listint(dlistint_t *head);
 size_t dlistint_len(const dlistint_t *h);
 size_t print_dlistint(const dlistint_t *h);
