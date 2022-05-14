653b112647722a21d024dd4ca53f18b06fb08110
miguecoder <mmarqzm@gmail.com>
2022-05-13 21:45:57 -0500

    function that inserts a new node at a given position.

---
+++ 0x17-doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,47 @@
+#include "lists.h"
+#include <stdio.h>
+/**
+ * insert_dnodeint_at_index - function that adds a new node at the beginning
+ * of a dlistint_t list.
+ * @head:pointer to a struct list
+ * @n: new integer add to a new node
+ * Return: The number of nodes
+ */
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	dlistint_t *new = malloc(sizeof(dlistint_t));
+	dlistint_t *tmp = *h;
+
+	if (new == NULL)
+	{
+		return (NULL);
+	}
+	else
+	{
+		if (idx == 0)
+		{
+			if (*h)
+			{
+				return (add_dnodeint(h, n));
+			}
+		}
+		new->n = n;
+		while (i < idx - 1 && tmp)
+		{
+			tmp = tmp->next;
+			i++;
+		}
+		if (tmp)
+		{
+			if (tmp->next)
+			{
+				tmp->next->prev = new;
+			}
+			new->next = tmp->next;
+			new->prev = tmp;
+			tmp->next = new;
+		}
+		return (new);
+	}
+}
--- 0x17-doubly_linked_lists/lists.h
+++ 0x17-doubly_linked_lists/lists.h
@@ -26,4 +26,5 @@ size_t dlistint_len(const dlistint_t *h);
 size_t print_dlistint(const dlistint_t *h);
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
 int sum_dlistint(dlistint_t *head);
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n);
 #endif
