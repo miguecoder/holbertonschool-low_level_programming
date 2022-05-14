75a8678e5af7ec37149b0c956e43ddd073ce5924
miguecoder <mmarqzm@gmail.com>
2022-05-13 20:26:19 -0500

    if (head == NULL)

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+/**
+ * get_dnodeint_at_index - unction that returns the nth node
+ * of a dlistint_t linked list.
+ * @head:pointer to a struct list
+ * @index:index of the node, starting from 0
+ * Return: The number of nodes
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	unsigned int node = 0;
+
+	if (head == NULL)
+	{
+		return (NULL);
+	}
+
+	while (node < index)
+	{
+		head = head->next;
+		node++;
+	}
+	return (head);
+}
